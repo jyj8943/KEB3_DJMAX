c7c871d7a5c3d181a3aa5dc8ebc58a7043a92983
jyj8943 <jyj8943@kyonggi.ac.kr>
2024-07-04 14:20:10 +0900

    그리드 상하 이동, 재생 구현

--- Assets/Script/InGamePlayScript/ChartReader.cs
+++ Assets/Script/InGamePlayScript/ChartReader.cs
@@ -1,24 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ChartReader : MonoBehaviour
 {
     public GameObject chart;
     public GameObject grids;
-    public Scrollbar scrollbar;
-    public float gridCount = 0;
+    public float gridCount;
+    public float maxGridCount;
 
-    // Start is called before the first frame update
-    void Start()
+    public bool isPlaying = false;
+    public float chartSpeed;
+
+    void Awake()
     {
-        
+        //초기화
+        gridCount = 0f;
+        maxGridCount = 60f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        chart.transform.position = new Vector3(0f, scrollbar.value, 0f);
+        //테스트용 다시시작 버튼
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Editor");
+        }
+
+        //그리드 한칸 올리기
+        if (Input.GetKeyDown(KeyCode.W) && gridCount <= maxGridCount)
+        {
+            grids.transform.Translate(0f, -8f, 0f);
+            gridCount++;
+        }
+        //그리드 한칸 내리기
+        if (Input.GetKeyDown(KeyCode.S) && gridCount > 0)
+        {
+            grids.transform.Translate(0f, 8f, 0f);
+            gridCount--;
+        }
+
+        //그리드 1배속으로 재생
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (isPlaying) 
+                isPlaying = false;
+            else 
+                isPlaying = true;
+        }
+        if (isPlaying)
+        {
+            grids.transform.Translate(0f, -chartSpeed * Time.deltaTime, 0f);
+        }
     }
 }
