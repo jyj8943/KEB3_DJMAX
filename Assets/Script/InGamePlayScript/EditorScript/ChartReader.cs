eb844fc50433d43bcb07a2b898ba54f57589e62f
jyj8943 <jyj8943@kyonggi.ac.kr>
2024-07-08 13:02:05 +0900

    7/8 에디터

--- Assets/Script/InGamePlayScript/EditorScript/ChartReader.cs
+++ Assets/Script/InGamePlayScript/EditorScript/ChartReader.cs
@@ -47,21 +47,21 @@ public class ChartReader : MonoBehaviour
         //if (isPlaying) gridList.transform.Translate(0f, -EditorManager.instance.defaultChartSpeed 
         //    * EditorManager.instance.userChartSpeed * Time.deltaTime, 0f);
 
-        // 마우스 휠에 따른 스크롤 기능 -> 메인카메라가 이동하도록 메인 카메라에 스크립트 작성
-        float wheelInput = Input.GetAxis("Mouse ScrollWheel"); 
-        if (wheelInput > 0 && gridCount < EditorManager.instance.maxGridCount )
-        {
-            gridList.transform.Translate(0f, -wheelInput * 3, 0f);
-        }
-        else if (wheelInput < 0 && gridCount >= 0 ) 
-        {
-            gridList.transform.Translate(0f, -wheelInput * 3, 0f);
-        }
+        //// 마우스 휠에 따른 스크롤 기능 -> 메인카메라가 이동하도록 메인 카메라에 스크립트 작성
+        //float wheelInput = Input.GetAxis("Mouse ScrollWheel"); 
+        //if (wheelInput > 0 && gridCount < EditorManager.instance.maxGridCount )
+        //{
+        //    gridList.transform.Translate(0f, -wheelInput * 3, 0f);
+        //}
+        //else if (wheelInput < 0 && gridCount >= 0 ) 
+        //{
+        //    gridList.transform.Translate(0f, -wheelInput * 3, 0f);
+        //}
 
-        //그리드 카운트 계산 -> 메인카메라가 이동하도록 메인 카메라에 스크립트 작성
-        gridCount = -gridList.transform.position.y / (8 * EditorManager.instance.userChartSpeed);
+        ////그리드 카운트 계산 -> 메인카메라가 이동하도록 메인 카메라에 스크립트 작성
+        //gridCount = -gridList.transform.position.y / (8 * EditorManager.instance.userChartSpeed);
 
-        // 유저의 배속에 따라 그리드의 높이 재구성
+        //// 유저의 배속에 따라 그리드의 높이 재구성
 
     }
 }
