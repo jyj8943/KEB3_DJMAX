10332049f11abe399bb4425dc96bbe2936fa6524
park020311 <80206892+park020311@users.noreply.github.com>
2024-07-29 14:51:16 +0900

    Auto stash before rebase of "ChanH" onto "origin/ChanH"
    에디터 블러처리

--- Assets/Script/InGamePlayScript/EditorScript/PauseCanvas.cs
+++ Assets/Script/InGamePlayScript/EditorScript/PauseCanvas.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
-
+using UnityEngine.SceneManagement;
 public class PauseCanvas : MonoBehaviour
 {
     public GameObject pauseCanvas;
@@ -11,24 +11,16 @@ public class PauseCanvas : MonoBehaviour
     public RawImage movieScreen;
     public VideoPlayer videoPlayer;
     public EditorCamera EditorCamera;
-
-    private MainCanvasBlurControl mainCanvasBlurControl; // 메인 캔버스 블러 제어 스크립트 참조
+    public GameObject volume;
+    
+    
+    
 
     public static bool isPauseCanvasOn = false;
     public static bool isHelpCanvasOn = false;
 
     void Start()
     {
-        // MainCanvasBlurControl 스크립트를 찾아서 참조
-        mainCanvasBlurControl = FindObjectOfType<MainCanvasBlurControl>();
-
-        // 블러 처리 제외 리스트에 pauseCanvas와 helpCanvas 추가
-        if (mainCanvasBlurControl != null)
-        {
-            mainCanvasBlurControl.excludeObjects.Add(pauseCanvas);
-            mainCanvasBlurControl.excludeObjects.Add(helpCanvas);
-        }
-
         pauseCanvas.SetActive(false);
         helpCanvas.SetActive(false);
     }
@@ -37,18 +29,7 @@ public class PauseCanvas : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // if (isHelpCanvasOn)
-            // {
-            //     OnClickCloseHelpBtn(); // HelpCanvas 상태에서는 ESC 키 입력 시 Return 버튼과 같은 동작
-            // }
-            // else if (isPauseCanvasOn)
-            // {
-            //     OnClickContinueBtn(); // PauseCanvas 상태에서는 ESC 키 입력 시 Continue 버튼과 같은 동작
-            // }
-            // else
-            // {
-            //     ActivatePauseCanvas();
-            // }
+            
             if (!isPauseCanvasOn)
             {
                 ActivatePauseCanvas();
@@ -78,29 +59,20 @@ public class PauseCanvas : MonoBehaviour
     private void ActivatePauseCanvas()
     {
         
-        if (mainCanvasBlurControl != null)
-        {
-            mainCanvasBlurControl.EnableBlur(); // 블러 활성화
-        }
+        
+        volume.SetActive(true); // 블러 활성화
         pauseCanvas.SetActive(true);
-
         Time.timeScale = 0f;
         videoPlayer.Pause();
-
         isPauseCanvasOn = true;
     }
 
     public void OnClickContinueBtn()
     {
         pauseCanvas.SetActive(false);
-        if (mainCanvasBlurControl != null)
-        {
-            mainCanvasBlurControl.DisableBlur(); // 블러 비활성화
-        }
-
+        volume.SetActive(false); // 블러 비활성화
         Time.timeScale = 1f;
         videoPlayer.Play();
-
         isPauseCanvasOn = false;
     }
 
@@ -134,5 +106,6 @@ public class PauseCanvas : MonoBehaviour
     public void OnClickExitBtn()
     {
         // Exit 버튼 클릭 시 메인메뉴로 가게끔 구현 예정
+        SceneManager.LoadScene("TitleMenu");
     }
 }
