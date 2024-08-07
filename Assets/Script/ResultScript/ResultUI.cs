eabfc10496e65d5f089003d527924f323c1967c4
jyj8943 <jyj8943@kyonggi.ac.kr>
2024-08-07 17:36:57 +0900

    8/7 ResultUI 수정

--- Assets/Script/ResultScript/ResultUI.cs
+++ Assets/Script/ResultScript/ResultUI.cs
@@ -1,22 +1,22 @@
-public TextMeshProUGUI scoreText;
-public TextMeshProUGUI comboText;
-public TextMeshProUGUI perfectCountText;
-public TextMeshProUGUI greatCountText;
-public TextMeshProUGUI goodCountText;
-public TextMeshProUGUI missCountText;
-
-private void Start()
-{
-    // 초기 UI 설정
-    UpdateUI();
-}
-
-private void UpdateUI()
-{
-    scoreText.text = $"Score: {tempScore:F0}";
-    comboText.text = $"Combo: {tempCombo}";
-    perfectCountText.text = $"Perfect: {perfectCount}";
-    greatCountText.text = $"Great: {greatCount}";
-    goodCountText.text = $"Good: {goodCount}";
-    missCountText.text = $"Miss: {missCount}";
-}
+// public TextMeshProUGUI scoreText;
+// public TextMeshProUGUI comboText;
+// public TextMeshProUGUI perfectCountText;
+// public TextMeshProUGUI greatCountText;
+// public TextMeshProUGUI goodCountText;
+// public TextMeshProUGUI missCountText;
+//
+// private void Start()
+// {
+//     // 초기 UI 설정
+//     UpdateUI();
+// }
+//
+// private void UpdateUI()
+// {
+//     scoreText.text = $"Score: {tempScore:F0}";
+//     comboText.text = $"Combo: {tempCombo}";
+//     perfectCountText.text = $"Perfect: {perfectCount}";
+//     greatCountText.text = $"Great: {greatCount}";
+//     goodCountText.text = $"Good: {goodCount}";
+//     missCountText.text = $"Miss: {missCount}";
+// }
