1514ddce11f9508b3e1ff02d8edc1057d4ebac17
chris <chris4364@inha.edu>
2024-08-13 17:39:36 +0900

    Sound Setting 일부 구현

--- Assets/Script/SettingScript/SoundSettingManager.cs
+++ Assets/Script/SettingScript/SoundSettingManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundSettingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
+    public TotalManager TM;
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
+    public AudioSource bgm;
+    public TextMeshProUGUI bgmLevel;
+    public Slider bgmSlider;
+    public static float initBgm = 0.5f;
+    
+    public AudioSource track;
+    public TextMeshProUGUI trackLevel;
+    public Slider trackSlider;
+    public static float initTrack = 0.5f;
+
+    public AudioSource sfx;
+    public TextMeshProUGUI sfxLevel;
+    public Slider sfxSlider;
+    public static float initSfx = 0.5f;
+
+    void Awake()
+    {
+        TM = TotalManager.instance;
+        InitVolume();
+    }
+
+    public void SetBgmVolume(float volume)
+    {
+        bgm.volume = volume;
+        bgmLevel.text = (volume*100).ToString("F0") + '%';
+        initBgm = volume;
+    }
+
+    public void SetTrackVolume(float volume)
+    {
+        // track.volume = volume;
+        trackLevel.text = (volume*100).ToString("F0") + '%';
+        initTrack = volume;
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        // sfx.volume = volume;
+        sfxLevel.text = (volume*100).ToString("F0") + '%';
+        initSfx = volume;
+    }
+
+    void InitVolume()
+    {
+        bgm.volume = initBgm;
+        bgmSlider.value = initBgm;
+        bgmLevel.text = (initBgm*100).ToString("F0") + '%';
+
+        // track.volume = initTrack;
+        trackSlider.value = initTrack;
+        trackLevel.text = (initTrack*100).ToString("F0") + '%';
+
+        // sfx.volume = initSfx;
+        sfxSlider.value = initSfx;
+        sfxLevel.text = (initSfx*100).ToString("F0") + '%';
+    }
+
 }
