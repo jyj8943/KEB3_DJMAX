2187eaf1aecfba88f6c91142cd0dee5d681de436
chris <chris4364@inha.edu>
2024-08-14 17:47:30 +0900

    Setting 에러 수정 중

--- Assets/Script/SettingScript/GameSettingManager.cs
+++ Assets/Script/SettingScript/GameSettingManager.cs
@@ -1,80 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
-using UnityEngine.EventSystems;
+// using System.Collections;
+// using System.Collections.Generic;
+// using TMPro;
+// using UnityEngine;
+// using UnityEngine.EventSystems;
 
-public class GameSettingManager : MonoBehaviour
-{
-    public TotalManager TM;
+// public class GameSettingManager : MonoBehaviour
+// {
+//     public TotalManager TM;
 
-    public GameObject syncBtn;
-    public TextMeshProUGUI sync;
-    public float sync_ = 1.0f;
+//     public GameObject syncBtn;
+//     public TextMeshProUGUI sync;
+//     private float sync_ = 1.0f;
 
-    public GameObject trackSpeedBtn;
-    public TextMeshProUGUI trackSpeed;
-    public float trackSpeed_ = 1.0f;
+//     public GameObject trackSpeedBtn;
+//     public TextMeshProUGUI trackSpeed;
+//     private float trackSpeed_ = 1.0f;
 
-    void Awake()
-    {
-        TM = TotalManager.instance;
-    }
+//     private float holdingTime = 0.15f;
+//     private float timer = 0f;
+//     private bool isHoldingUp = false;
+//     private bool isHoldingDown = false;
 
-    void Start()
-    {
-        sync.text = sync_.ToString("F1");
-        trackSpeed.text = trackSpeed_.ToString("F1");
-    }
+//     void Awake()
+//     {
+//         TM = TotalManager.instance;
+//     }
 
-    void Update()
-    {
-        GameObject selectedObj = EventSystem.current.currentSelectedGameObject;
+//     void Start()
+//     {
+//         sync.text = sync_.ToString("F1");
+//         trackSpeed.text = trackSpeed_.ToString("F1");
+//     }
 
-        if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if(selectedObj == syncBtn)
-            {
-                SetSyncUp();
-            }
-            else if(selectedObj == trackSpeedBtn)
-            {
-                trackSpeed_ += 0.1f;
-                trackSpeed.text = trackSpeed_.ToString("F1");
-            }
-        }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if(selectedObj == syncBtn)
-            {
-                SetSyncDown();
-            }
-            else if(selectedObj == trackSpeedBtn)
-            {
-                trackSpeed_ -= 0.1f;
-                trackSpeed.text = trackSpeed_.ToString("F1");
-            }
-        }
-    }
+//     void Update()
+//     {
+//         GameObject selectedObj = EventSystem.current.currentSelectedGameObject;
 
-    void SetSyncUp()
-    {
-        sync_ += 1f;
-        sync.text = sync_.ToString("F1");
-    }
+//         if(Input.GetKeyDown(KeyCode.RightArrow))
+//         {
+//             if(selectedObj == syncBtn)
+//             {
+//                 SetSyncUp();
+//             }
+//             else if(selectedObj == trackSpeedBtn)
+//             {
+                
+//             }
+//         }
+//         else if(Input.GetKeyDown(KeyCode.LeftArrow))
+//         {
+//             if(selectedObj == syncBtn)
+//             {
+//                 SetSyncDown();
+//             }
+//             else if(selectedObj == trackSpeedBtn)
+//             {
+//                 trackSpeed_ -= 0.1f;
+//                 trackSpeed.text = trackSpeed_.ToString("F1");
+//             }
+//         }
+//     }
 
-    void SetSyncDown()
-    {
-        sync_ -= 1f;
-        sync.text = sync_.ToString("F1");
-    }
+//     void SetSyncUp()
+//     {
+//         sync_ += 1f;
+//         sync.text = sync_.ToString("F1");
+//     }
 
-    void SetTrackSpeedUp()
-    {
-    }
+//     void SetSyncDown()
+//     {
+//         sync_ -= 1f;
+//         sync.text = sync_.ToString("F1");
+//     }
 
-    void SetTrackSpeedDown()
-    {
-    }
+//     void SetTrackSpeedUp()
+//     {
+//     }
 
-}
+//     void SetTrackSpeedDown()
+//     {
+//     }
+
+// }
